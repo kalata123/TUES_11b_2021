ad767a37ea23f69849f6fea7ef1b88c4025ddbf4
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-10-13 11:01:47 +0300

    [Home] [Finished TP homework]

--- TP/hw_1/accumulate.py
+++ TP/hw_1/accumulate.py
@@ -1,9 +1,9 @@
-import pdb
-
-
 def accumulate_left(func, a, b):
-    pdb.set_trace()
-    if b == []:
-        return 1
+    if b:
+        return accumulate_left(func, func(a, b[0]), b[1:])
     else:
-        return a/accumulate_left(func, a, b[:-1])
+        return a
+
+
+def accumulate_right(func, a, b):
+    return func(b[0], accumulate_right(func, a, b[1:])) if b else a
