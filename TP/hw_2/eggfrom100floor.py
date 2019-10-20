5179a0932c459e58ad43b3b3009c9c1a09fa0ee0
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-10-20 10:58:50 +0300

    [At home] [TP hw is not working properly]

--- TP/hw_2/eggfrom100floor.py
+++ TP/hw_2/eggfrom100floor.py
@@ -2,16 +2,13 @@ def drop_breaks(floor, kill_floor):
     return 1 if floor >= kill_floor else 0
 
 
-def main(kill_floor=99):
-    building_floors = 100
-    # eggs = 2
+def main(kill_floor=99, building_floors=100):
     j = 0
     for i in range(0, building_floors, 14):
-        i -= j  # max-floor # 39 from 27 to 39 = 12 = i
+        i -= j
         if drop_breaks(i, kill_floor):
             a = i - (14 - j)
             while (a < i):
-                # pdb.set_trace()
                 a = i - (14 - j)
                 if drop_breaks(a, kill_floor):
                     return a
@@ -20,4 +17,4 @@ def main(kill_floor=99):
 
 
 if __name__ == "__main__":
-    print(f"kill_floor is {main(45)}")
+    print(f"kill_floor is {main(91)}")
