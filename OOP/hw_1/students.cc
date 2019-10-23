f03548326511e2bd5e4e5fb0df2daf618802d2ad
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-10-23 11:26:44 +0300

    a bit more OOP hw

--- OOP/hw_1/students.cc
+++ OOP/hw_1/students.cc
@@ -1,8 +1,25 @@
 #include "iostream"
+#include "list"
 using namespace std;
 
 class StudentInfo {
+    private:
+        string name;
+        list<double> marks;
 
+    public:
+
+        string getName(){
+            return this->name;
+        }
+
+        list<double> getMarks(){
+            return this->marks;
+        }
+
+        void addMark(double new_mark){
+            this->marks.push_back(new_mark);
+        }
 
 };
 
