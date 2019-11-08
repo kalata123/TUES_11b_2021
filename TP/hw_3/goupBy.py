a2d8e027bf8227c574ac1a9afda5536d13579ad8
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-11-08 10:49:53 +0200

    [Homework] [first problem done]

--- TP/hw_3/goupBy.py
+++ TP/hw_3/goupBy.py
@@ -1,8 +1,16 @@
+from collections import defaultdict
+``
+
 def group_by_f(func, lst):
-    dct = {}
+    dct = defaultdict(list)
     for i in range(len(lst)):
-        if func(lst[i]) in dct.keys()= func(lst[j]):
         dct[func(lst[i])].append(lst[i])
     return dct
-dct = group_by_f(lambda a: a % 2 == 0, [1,2,3,4,5,6,7,8,9,10])
-print(dct)
+
+
+if __name__ == '__main__':
+    a = [[1], [7, 8], [1, 2, 3, 4], [4], ["random", "words"]]
+    dct = group_by_f(len, a)
+    print(dct.keys())
+    print(dct.values())
+    print(dct)
