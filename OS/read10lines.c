d2629ca83070f2b5e9ab76ee16206b0d95a746e4
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-11-16 21:38:35 +0200

    [Home] [Done OS HW_2]

--- OS/read10lines.c
+++ OS/read10lines.c
@@ -2,20 +2,31 @@
 #include "sys/stat.h"
 #include "fcntl.h"
 #include "unistd.h"
+#include "string.h"
 
-int main(){
-    int fd = open("read10lines.c", O_RDONLY);
-    char buff[200];
+void mywrite(int fd){
+    char buff;
     int status;
     int nls = 0;
-    while ((status = read(fd, buff, 1)) != 0){
-        int written = 0;
-        while (written < status)
-        {
-            if (buff[0] == '\n') ++nls;
-            written += write(fd, buff, status - written);
+    while (nls < 10){
+        status = read(fd, &buff, 1);
+        if (buff == '\n') nls++;
+        write(STDOUT_FILENO, &buff, status);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    for(int i = 1; i < argc; ++i){
+        if(i != 1) write(STDOUT_FILENO, "\n", 1);
+        fd = open(argv[i], O_RDONLY);
+        if(argc > 2){
+            write(STDOUT_FILENO, "==> ", 4);
+            write(STDOUT_FILENO, argv[i], strlen(argv[i]));
+            write(STDOUT_FILENO, " <==\n", 5);
         }
-        
+        mywrite(fd);
     }
     return 0;
 }
