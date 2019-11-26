05f51f507447338b442b4b63608d3b13075b9088
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-11-26 09:55:52 +0200

    [Home] [little TP and some more OS hw]

--- OS/makefile/main.c
+++ OS/makefile/main.c
@@ -4,48 +4,82 @@
 #include "sys/stat.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
+#define stdin_name "stdin"
+#define o_header "==> "
+#define c_header " <==\n"
 
+void headers();
 void readStdIn(int count);
-int myread(int fd);
+int myread_fd(int fd);
 void mywrite(int count, int fd, int flag);
+int myread_str(char *str);
 
 int main(int argc, char const *argv[])
 {
-    for (int i = 1; i < argc; ++i){
-        if (atoi(argv[i]) == '-'){
-            // read from stdin
-            readStdIn(argc);
-        }
-        else{
-            int fd;
-            fd = open(argv[i], O_RDONLY);
-            mywrite(argc, fd, myread(fd));
-            close(fd);
+    if (argc == 1) readStdIn(argc);
+    else {
+        for (int i = 1; i < argc; ++i){
+            if(i != 1) write(STDOUT_FILENO, "\n", 1); // writes \n before ==>
+            if (atoi(argv[i]) == '-' ){
+                // read from stdinif (argc > 2){
+                if (argc > 2) headers(stdin_name); // puts headers
+                readStdIn(argc);
+            }
+            else{
+                int fd;
+                if (argc > 2) headers(argv[i]); // puts headers
+                fd = open(argv[i], O_RDONLY);
+                mywrite(argc, fd, myread_fd(fd));
+                close(fd);
+            }
         }
     }
     return 0;
 }
 
+
+void headers(char* str){
+    write(STDOUT_FILENO, o_header, strlen(o_header));
+    write(STDOUT_FILENO, str, strlen(str));
+    write(STDOUT_FILENO, c_header, strlen(c_header));
+}
+
+
 void readStdIn(int count){
     char buff;
-    int status = 0;
+    char str[256];
+    int status = 0, i = 0, nls;
     while ((status = read(STDIN_FILENO, &buff, 1) != 0))
     {
-        write(STDOUT_FILENO, &buff, status);
+        str[i] = buff;
+        i++;
     }
+    nls = myread_str(buff); // fails here
+    printf("%d\n", nls);
+    write(STDOUT_FILENO, &buff, status);
     
 }
 
-int myread(int fd){
+
+int myread_str(char *str){
+    int nls = 0, len = strlen(str);
+    for (int i = 0; i < len; i++){
+        if (str[i] == '\n') nls ++;
+    }
+    return nls;
+}
+
+
+int myread_fd(int fd){
     char buff;
     int status = -1;
     int nls = 0;
-    while (read(fd, &buff, 1) != 0){
-        if (buff == '\n') nls++;
-    }
+    while (read(fd, &buff, 1) != 0) if (buff == '\n') nls++; // counts \n
     if (nls <= 10) return -1; // 10 or less lines
     return nls; // more than 10 lines
 }
+
 
 void mywrite(int count, int fd, int flag){
     char buff;
@@ -55,8 +89,8 @@ void mywrite(int count, int fd, int flag){
         read(fd, &buff, 1);
         if (buff == '\n') flag--;
     }
-    while ((status = read(fd, &buff, 1)) != 0){ // writes everything for all of them
+    while ((status = read(fd, &buff, 1)) != 0){ // writes everything left
         write(STDOUT_FILENO, &buff, status);
     }
-    write(STDOUT_FILENO, "\n", status);
+    write(STDOUT_FILENO, "\n", 1);
 }
