fad230cd841ba1e93f0fe73b8e6350210b4835b1
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-11-29 00:54:52 +0200

    [Home] [OS... Got me 250 pts... no more, no err_handling]

--- OS/makefile/main.c
+++ OS/makefile/main.c
@@ -5,7 +5,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
-#define stdin_name "stdin"
+#define stdin_name "standard input"
 #define o_header "==> "
 #define c_header " <==\n"
 
@@ -17,9 +17,24 @@ int myread_str(char *str);
 
 int main(int argc, char const *argv[])
 {
+    struct stat errs;    
     if (argc == 1) readStdIn(); // zero argvs
     else {
         for (int i = 1; i < argc; ++i){
+            if (S_ISDIR(errs.st_mode)){
+                write(STDERR_FILENO, "tail: error reading '", 21);
+                write(STDERR_FILENO, argv[i], strlen(argv[i]));
+                write(STDERR_FILENO, "': Is a directory\n", 15);
+                perror("");
+                continue;
+            }
+            else if(stat(argv[i], &errs) != 0){
+                write(STDERR_FILENO, "tail: cannot open '", 21);
+                write(STDERR_FILENO, argv[i], strlen(argv[i]));
+                write(STDERR_FILENO, "' for reading:", 15);
+                perror("");
+                continue;
+            }
             if(i != 1) write(STDOUT_FILENO, "\n", 1); // writes \n before ==>
             if (strcmp(argv[i], "-") == 0){
                 if (argc > 2) headers(stdin_name); // puts headers
@@ -35,13 +50,6 @@ int main(int argc, char const *argv[])
         }
     }
     return 0;
-}
-
-
-void headers(char* str){
-    write(STDOUT_FILENO, o_header, strlen(o_header));
-    write(STDOUT_FILENO, str, strlen(str));
-    write(STDOUT_FILENO, c_header, strlen(c_header));
 }
 
 
@@ -65,6 +73,27 @@ void readStdIn(){
 }
 
 
+void mywrite(int count, int fd, int flag){
+    char buff;
+    int status = 0;
+    lseek(fd, 0, SEEK_SET);
+    while(flag >= 11){ // goes in if only flag > 10
+        read(fd, &buff, 1);
+        if (buff == '\n') flag--;
+    }
+    while ((status = read(fd, &buff, 1)) != 0){ // writes everything left
+        write(STDOUT_FILENO, &buff, status);
+    }
+}
+
+
+void headers(char* str){
+    write(STDOUT_FILENO, o_header, strlen(o_header));
+    write(STDOUT_FILENO, str, strlen(str));
+    write(STDOUT_FILENO, c_header, strlen(c_header));
+}
+
+
 int myread_str(char *str){
     int nls = 0, len = strlen(str);
     for (int i = 0; i < len; i++){
@@ -79,21 +108,7 @@ int myread_fd(int fd){
     int status = -1;
     int nls = 0;
     while (read(fd, &buff, 1) != 0) if (buff == '\n') nls++; // counts \n
+    // printf("\n--myread_fd nl:\n%d is flag nl:", nls);
     if (nls <= 10) return -1; // 10 or less lines
     return nls; // more than 10 lines
-}
-
-
-void mywrite(int count, int fd, int flag){
-    char buff;
-    int status = 0;
-    lseek(fd, 0, SEEK_SET);
-    while(flag >= 10){ // goes in if only flag > 10
-        read(fd, &buff, 1);
-        if (buff == '\n') flag--;
-    }
-    while ((status = read(fd, &buff, 1)) != 0){ // writes everything left
-        write(STDOUT_FILENO, &buff, status);
-    }
-    write(STDOUT_FILENO, "\n", 1);
 }
