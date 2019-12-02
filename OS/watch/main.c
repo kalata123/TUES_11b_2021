390b6f9498e84901b38bff7029d6c0764295f88d
kalata123 <kaloyan.angelov.tues@gmail.com>
2019-12-02 10:48:43 +0200

    [In class] [implementing wait]

---
+++ OS/watch/main.c
@@ -0,0 +1,32 @@
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/wait.h"
+#include "string.h"
+#include "stdio.h"
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    int status = 0;
+    while (1)
+    {
+        pid = fork();
+        if (pid < 0){
+            write(STDERR_FILENO, "fork", 4);
+            perror("");
+            return 1;
+        }else if (pid == 0){
+            if(execvp(argv[1], &argv[1]) == -1){
+                perror(argv[1]);
+                return 1;
+            }
+        }else
+        {
+            if (waitpid(pid, &status, 0) != pid){
+                perror("");
+                return 1;
+            }
+            sleep(2);
+        }
+    }
+    return 0;
+}
