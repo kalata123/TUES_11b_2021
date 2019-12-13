b1e61ec3f7a292b020d1a4c30ff37417eb0539a2
Kaloyan Angelov <kaloyan.angelov.tues@gmail.com>
2019-12-13 19:45:05 +0200

    [Home] [Shell ongoing]

--- OS/shell/shell.c
+++ OS/shell/shell.c
@@ -5,24 +5,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-char ** parse_inp(char*str){
-    char ** res = (char **) malloc(sizeof(char**));
-    char tmp[200] = "\0";
-    for (int i = 0, j = 0; i < strlen(str); ++i, ++j){
-        while (str[i] != ' ')
-        {
-            tmp[strlen(tmp)] = str[i];
-            ++i;
-            if (strlen(str) <= i) break;
-        }
-        res = (char **)realloc(res, sizeof(char**)*(strlen(tmp)));
-        strcpy(res[j], tmp);
-        tmp[0] = '\0';
-    }
-    return res;
-    free(*res);
-    free(res);
-}
+char** parse_cmdline( const char* cmdline );
 
 int main(int argc, char *argv[]) {
     pid_t pid;
@@ -43,7 +26,7 @@ int main(int argc, char *argv[]) {
         if (c == -1) break;
         str[strlen(str)] = '\0';
         printf("%s\n", str);
-        char **give = parse_inp(str);
+        char **give = parse_cmdline(str);
         pid = fork();
         if (pid < 0){
             perror("fork");
@@ -62,4 +45,22 @@ int main(int argc, char *argv[]) {
         free(str);
     }
     return 0;
+}
+char** parse_cmdline(const char* cmdline){
+    char ** res = (char **) malloc(sizeof(char**));
+    char tmp[200] = "\0";
+    for (int i = 0, j = 0; i < strlen(cmdline); ++i, ++j){
+        while (cmdline[i] != ' ')
+        {
+            tmp[strlen(tmp)] = cmdline[i];
+            ++i;
+            if (strlen(cmdline) <= i) break;
+        }
+        res = (char **)realloc(res, sizeof(char**)*(strlen(tmp)));
+        strcpy(res[j], tmp);
+        tmp[0] = '\0';
+    }
+    return res;
+    free(*res);
+    free(res);
 }
