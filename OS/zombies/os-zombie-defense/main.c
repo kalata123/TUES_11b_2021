977d6c96a7e9895207296cabdd04cc5402c6e2ec
kalata123 <kaloyan.angelov.tues@gmail.com>
2020-03-22 19:22:04 +0200

    [Home] HW Zombies game working, Grader - 52 ;(

--- OS/zombies/os-zombie-defense/main.c
+++ OS/zombies/os-zombie-defense/main.c
@@ -8,52 +8,38 @@ void *create_solder(void *);
 void *create_miner();
 void *create_zombies();
 
-int zombies = 1, soldiers = 0, lives = 100, gold = 100;
+int ZOMBIES = 1, SOLDIERS = 0, LIVES = 100, GOLD = 100;
 
 pthread_mutex_t mutex;
 
 int main() {
 	init();
-	print_gold(gold);
-	print_soldiers(soldiers);
-	print_zombies(5, zombies);
-	print_health(lives);
+	print_gold(GOLD);
+	print_soldiers(SOLDIERS);
+	print_health(LIVES);
 
 	pthread_mutex_init(&mutex, NULL);
 
 	pthread_t zombie_thread;
 	pthread_create(&zombie_thread, NULL, create_zombies, NULL);
+
 	while(1) {
-		int ch;
-		ch = lives ? get_input() : 'q';
+		int ch = get_input();
 		switch(ch) {
 			case 'q':
-				game_end(zombies);
+				game_end(ZOMBIES);
 				break;
 			case 'm':;
 				pthread_t miner_thread;
-				printf("%c\n",ch);
-				// if (gold >= 100){
-				// 	pthread_mutex_lock(&mutex);
-				// 	gold -= 100;
-				// 	print_gold(gold);
-				// 	pthread_mutex_unlock(&mutex);
-				// 	pthread_create(&miner_thread, NULL, create_miner, NULL);
-				// }
+				pthread_create(&miner_thread, NULL, create_miner, NULL);
 				break;
 			case 'x':;
 				pthread_t x10_soldier_thread;
-				if (gold >= 100){
-					pthread_create(&x10_soldier_thread, NULL, create_solder, (void *)10);
-					print_msg("asd");
-				}
+				pthread_create(&x10_soldier_thread, NULL, create_solder, (void *)10);
 				break;
 			case 's':;
 				pthread_t soldier_thread;
-				print_msg("dsa");
-				if (gold >= 10){
 				pthread_create(&soldier_thread, NULL, create_solder, (void *)1);
-				}
 				break;
 		}
 	}
@@ -64,57 +50,75 @@ int main() {
 
 void *create_solder(void * num){
 	long soldiers_count = (long) num;
-	if (soldiers_count == 1){
+	if (soldiers_count == 1 && GOLD >= 10){
+		print_msg("Soldier created!");
 		pthread_mutex_lock(&mutex);
-		soldiers += 1;
-		gold -= 10;
-		print_gold(gold);
-		print_soldiers(soldiers);
+			SOLDIERS += 1;
+			print_soldiers(SOLDIERS);
+			GOLD -= 10;
+			print_gold(GOLD);
 		pthread_mutex_unlock(&mutex);
-	}else
-	{
+	} else if (soldiers_count == 10 && GOLD >= 100){
+		print_msg("10 x soldier created!");
 		pthread_mutex_lock(&mutex);
-		soldiers += 10;
-		gold -= 100;
-		print_gold(gold);
-		print_soldiers(soldiers);
+			SOLDIERS += 10;
+			GOLD -= 100;
+			print_gold(GOLD);
+			print_soldiers(SOLDIERS);
 		pthread_mutex_unlock(&mutex);
+	} else{
+		print_fail("Not enough gold!");
 	}
 	
 }
 
 void *create_miner(){
-	while (1)
-	{
+	if (GOLD >= 100){
+		print_msg("Miner created!");
 		pthread_mutex_lock(&mutex);
-		printf("miner digging\n");
-		gold += 10;
-		print_gold(gold);
+			GOLD -= 100;
+			print_gold(GOLD);
 		pthread_mutex_unlock(&mutex);
-		sleep(1);
+		while (1){
+			pthread_mutex_lock(&mutex);
+				GOLD += 10;
+				print_gold(GOLD);
+			pthread_mutex_unlock(&mutex);
+			sleep(1);
+		}
+	} else
+	{
+		print_fail("Not enough gold!");
 	}
 	
 }
 
 void *create_zombies(){
-	while (1)
-	{
-		pthread_mutex_lock(&mutex);
+	while (1){
 		for (int i = 5; i >= 0; --i){
-			print_zombies(i, zombies);
+			pthread_mutex_lock(&mutex);
+				print_zombies(i, ZOMBIES);
+			pthread_mutex_unlock(&mutex);
 			sleep(1);
 		}
 
-		if (zombies > soldiers){
-			lives -= (zombies - soldiers);
+		if (ZOMBIES > SOLDIERS){
+			pthread_mutex_lock(&mutex);
+				LIVES -= (ZOMBIES - SOLDIERS);
+			pthread_mutex_unlock(&mutex);
 			print_fail("Zombie attack succeded ;(!");
-			print_health(lives);
-			if (lives <= 0) game_end(zombies);
+			if (LIVES <= 0) game_end(ZOMBIES);
+			else {
+				pthread_mutex_lock(&mutex);
+					print_health(LIVES);
+				pthread_mutex_unlock(&mutex);
+			}
 		}else {
 			print_succ("Zombie attack deflected!:)");
 		}
 
-		zombies *= 2;
+		pthread_mutex_lock(&mutex);
+			ZOMBIES *= 2;
 		pthread_mutex_unlock(&mutex);
 	}
 }
