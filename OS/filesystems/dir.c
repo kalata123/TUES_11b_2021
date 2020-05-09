44f2dcd2f8fc1b691df00e527875b35cd0b97a0f
kalata123 <kaloyan.angelov.tues@gmail.com>
2020-05-09 19:57:51 +0300

    [Home] OS, ls 450 pts, no errhandling, -R and mutiple files/directories

--- OS/filesystems/dir.c
+++ OS/filesystems/dir.c
@@ -1,4 +1,7 @@
 #include <dirent.h>
+#include <errno.h>
+#include <getopt.h>
+#include <grp.h>
 #include <pwd.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,76 +10,172 @@
 #include <time.h>
 #include <unistd.h>
 
+#define FLAGS "lRA"
+
 typedef struct dirent dirent_t;
 typedef struct stat stat_t;
+typedef struct passwd passwd_t;
+typedef struct group group_t;
 
 char *formatdate(char *str, time_t val);
+void colorize(char clr[2], int bold);
+int scan_dir(DIR *dir, int argc, char **argv, char *name);
+int is_chr_in_str(char *, char);
+char get_file_type(mode_t type);
+void print_date();
 
 int main(int argc, char **argv) {
-    DIR *dir = opendir(".");
-    if (argc == 2) {
-        dir = opendir(argv[1]);
-    }
-    if (dir == NULL) {
-        perror("opendir");
-        return -1;
-    }
 
-    dirent_t *dircontent;
-    stat_t statcontent;
-    printf("%-20s", "Access Time");
-    printf("%-6s", "Uid");
-    printf("%-6s", "Size");
-    printf("%-10s\n", "Name");
-    while ((dircontent = readdir(dir)) != NULL) {
-        // exlude the pointers
-        if ((strcmp(dircontent->d_name, "..") != 0) && (strcmp(dircontent->d_name, ".") != 0)) {
-
-            // get the status of the current object
-            if (stat(dircontent->d_name, &statcontent) == -1) {
-                perror("stat");
-                return -1;
-            }
-
-            // check flags
-            char *flags = "la";
-
-            char str[36];
-            printf("\033[0;34m");
-            printf("%-20s", formatdate(str, statcontent.st_atime));
-            printf("\033[0m");
-
-            printf("\033[0;31m");
-            printf("%-6u", statcontent.st_uid);
-            printf("\033[0m");
-
-            printf("\033[0;32m");
-            printf("%-6ld", statcontent.st_size);
-            printf("\033[0m");
-
-            // print colorized name
-
-            if (S_ISDIR(statcontent.st_mode)) {
-                printf("\033[1;32m");
-                printf("%-10s\n", dircontent->d_name);
-                printf("\033[0m");
+    DIR *dir = NULL;
+    int val = 1;
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            if (argv[i][0] != '-') {
+                val = 0;
+                if ((dir = opendir(argv[i])) == NULL) {
+                    perror("opendir");
+                    return -1;
+                }
+                scan_dir(dir, argc, argv, ".");
+                if (closedir(dir) == -1) {
+                    perror("closedir");
+                    return -1;
+                }
             } else {
-                printf("\033[0;33m");
-                printf("%-10s", dircontent->d_name);
-                printf("\033[0m");
+                val = 1;
             }
-            printf("\n");
+        }
+    }
+    if (val) {
+        dir = opendir(".");
+        scan_dir(dir, argc, argv, ".");
+        if (closedir(dir) == -1) {
+            perror("closedir");
+            return -1;
         }
     }
 
-    if (closedir(dir) == -1) {
-        perror("closedir");
-        return -1;
-    }
     return 0;
 }
 
+int scan_dir(DIR *dir, int argc, char **argv, char *name) {
+
+    /* get all flags in one string */
+    // ->
+    int i = 0, checker = 0;
+    char option;
+    char flags[4] = {0};
+
+    while ((option = getopt(argc, argv, FLAGS)) != -1) {
+        for (i = 0, checker = 1; i < strlen(flags); i++) {
+            if (flags[i] == option) {
+
+                checker = 0;
+                break;
+            }
+        }
+        if (checker) {
+            if (is_chr_in_str(FLAGS, option)) {
+                flags[i] = option;
+            }
+        }
+        flags[3] = '\0';
+    }
+    // <-
+
+    /* for each dir get its info and do stuff */
+    // ->
+    unsigned int total_blocks = 0;
+    if (is_chr_in_str(flags, 'R')) {
+        printf("%s:\n", name);
+    }
+    dirent_t *dirinfo;
+    errno = 0;
+    while ((dirinfo = readdir(dir)) != NULL) {
+        stat_t statinfo;
+        if (stat(dirinfo->d_name, &statinfo) == -1) {
+            perror("stat");
+            return -1;
+        }
+
+        // skip procces if . or .. and flagg is not 'a'
+        if ((((strcmp(dirinfo->d_name, "..") == 0) ||
+              (strcmp(dirinfo->d_name, ".") == 0)) &&
+             !is_chr_in_str(flags, 'a')) ||
+            (dirinfo->d_name[0] == '.' &&
+             !is_chr_in_str(flags, 'A'))) {
+            continue;
+        } else {
+
+            total_blocks += statinfo.st_blocks;
+        }
+
+        if (is_chr_in_str(flags, 'l')) {
+            // print all things
+            printf("%c", get_file_type(statinfo.st_mode));
+            printf((statinfo.st_mode & S_IRUSR) ? "r" : "-");
+            printf((statinfo.st_mode & S_IWUSR) ? "w" : "-");
+            printf((statinfo.st_mode & S_IXUSR) ? "x" : "-");
+            printf((statinfo.st_mode & S_IRGRP) ? "r" : "-");
+            printf((statinfo.st_mode & S_IWGRP) ? "w" : "-");
+            printf((statinfo.st_mode & S_IXGRP) ? "x" : "-");
+            printf((statinfo.st_mode & S_IROTH) ? "r" : "-");
+            printf((statinfo.st_mode & S_IWOTH) ? "w" : "-");
+            printf((statinfo.st_mode & S_IXOTH) ? "x " : "- ");
+            printf("%ld ", statinfo.st_nlink);
+            passwd_t *pwd = getpwuid(statinfo.st_uid);
+            group_t *grp = getgrgid(statinfo.st_gid);
+
+            if (pwd->pw_name) {
+                printf("%s ", pwd->pw_name);
+            } else {
+
+                printf("%d ", statinfo.st_uid);
+            }
+            if (grp->gr_name) {
+                printf("%s ", grp->gr_name);
+            } else {
+                printf("%d ", statinfo.st_gid);
+            }
+            printf("%ld ", statinfo.st_size);
+            char str[36];
+            printf("%s", formatdate(str, statinfo.st_mtime));
+        }
+        if (!is_chr_in_str(flags, 'l')) {
+            printf("%c ", get_file_type(statinfo.st_mode));
+        } else {
+            printf(" ");
+        }
+
+        printf("%s\n", dirinfo->d_name);
+    }
+    if (errno) {
+        perror("readdir");
+    }
+
+    if (is_chr_in_str(flags, 'l')) {
+        printf("total %u\n", total_blocks / 2);
+    }
+    // <-
+}
+
+// helpful functions
+char get_file_type(mode_t type) {
+    return S_ISSOCK(type) ? 's' : S_ISDIR(type) ? 'd' : S_ISLNK(type) ? 'l' : S_ISREG(type) ? '-' : S_ISBLK(type) ? 'b' : S_ISCHR(type) ? 'c' : S_ISFIFO(type) ? 'p' : '?';
+}
+
 char *formatdate(char *str, time_t val) {
-    strftime(str, 36, "%d.%m.%Y %H:%M:%S", localtime(&val));
+    time_t time_now;
+    time(&time_now);
+    if (difftime(time_now, val) > 15778463) {
+        strftime(str, 36, "%b %d %Y", localtime(&val));
+    } else {
+        strftime(str, 36, "%b %d %H:%M", localtime(&val));
+    }
+
     return str;
+}
+
+int is_chr_in_str(char *flags, char wanted) {
+    return strchr(flags, wanted) != NULL;
 }
---
+++ OS/filesystems/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    printf(0 ? 0 ? "sad" : "it is here" : 1 ? 0 ? "yes" : "it is not here" : "asd");
+    return 0;
+}
